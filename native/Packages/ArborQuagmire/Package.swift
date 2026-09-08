ce34638bbc35763dd20c9673ea3a45f079075dfb
Joe Edelman <joe.edelman@gmail.com>
2026-09-08 00:44:27 -0400

    chore(native): update Quagmire to 0.6.0

--- native/Packages/ArborQuagmire/Package.swift
+++ native/Packages/ArborQuagmire/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .package(path: "../ArborClient"),
         .package(path: "../ArborKit"),
         .package(path: "../ArborProviders"),
-        .package(url: "https://github.com/jxe/quagmire.git", exact: "0.5.3")
+        .package(url: "https://github.com/jxe/quagmire.git", exact: "0.6.0")
     ],
     targets: [
         .target(
